ee15ca25efa1c3a8581cba9fdb9ccea98d1e4e04
Julius Rajala <juliusrajala@gmail.com>
2020-01-26 15:14:22 +0200

    Fixed faulty init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,7 +1,7 @@
-CREATE TABLE public.plant {
+CREATE TABLE public.plant (
   plant_id character varying(25) PRIMARY KEY NOT NULL,
   name character varying(25) NOT NULL,
   added_datetime timestamp with time zone NOT NULL DEFAULT NOW(),
   last_watered timestamp with time zone,
   days_to_wait numeric NOT NULL DEFAULT 1
-};
+);
